b615ff34fefdbcd37cf72be507b276abbf24a7fa
Галина Макенова <makenova11@mail.ru>
2023-02-24 00:11:49 +0700

    Добавлены интерфейсы репозиториев

---
+++ Interfaces/IBalanceRepository.cs
@@ -0,0 +1,31 @@
+﻿namespace FinancialSettlementService.Interfaces
+{
+    /// <summary>
+    /// Репозиторий работы с балансом клиента.
+    /// </summary>
+    public interface IBalanceRepository
+    {
+        /// <summary>
+        /// Проверить баланс счёта.
+        /// </summary>
+        /// <param name="clientId"> Идентификатор клиента. </param>
+        /// <returns> Баланс счёта. </returns>
+        Task<decimal?> CheckBalanceAsync(Guid clientId);
+
+        /// <summary>
+        /// Пополнить баланс счёта.
+        /// </summary>
+        /// <param name="clientId"> Идентификатор клиента. </param>
+        /// <param name="amount"> Сумма пополнения. </param>
+        /// <returns></returns>
+        Task DepositIntoAsync(Guid clientId, decimal amount);
+
+        /// <summary>
+        /// Снять деньги со счёта.
+        /// </summary>
+        /// <param name="clientId"> Идентификатор клиента. </param>
+        /// <param name="amount"> Сумма снятия. </param>
+        /// <returns></returns>
+        Task WithDrawFromAsync(Guid clientId, decimal amount);
+    }
+}
---
+++ Interfaces/IClientRepository.cs
@@ -0,0 +1,25 @@
+﻿using FinancialSettlementService.Dtos;
+
+namespace FinancialSettlementService.Interfaces
+{
+    /// <summary>
+    /// Репозиторий действий с клиентом.
+    /// </summary>
+    public interface IClientRepository
+    {
+        /// <summary>
+        /// Регистрация клиента
+        /// </summary>
+        /// <param name="clientDto"> Данные добавляемого пользователя. </param>
+        /// <param name="cancellationToken"> Структура отмены операций между потоками. </param>
+        /// <returns> Task. </returns>
+        Task SignUpAsync(ClientDto clientDto, CancellationToken cancellationToken);
+
+        /// <summary>
+        /// Получить клиента по id.
+        /// </summary>
+        /// <param name="id"> Идентификатор клиента.</param>
+        /// <returns> Информация о клиенте. </returns>
+        Task<ClientDto> GetByIdAsync(Guid id);
+    }
+}
