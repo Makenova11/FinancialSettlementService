a8c0c3760d9a98956bdef3c4e28628fa2512bc86
Галина Макенова <makenova11@mail.ru>
2023-02-24 00:12:22 +0700

    Добавлены репозитории

---
+++ Repositories/BalanceRepository.cs
@@ -0,0 +1,33 @@
+﻿using FinancialSettlementService.Interfaces;
+using FinancialSettlementService.Models;
+
+namespace FinancialSettlementService.Repositories
+{
+    /// <inheritdoc cref="IBalanceRepository"/>
+    public class BalanceRepository : IBalanceRepository
+    {
+        private readonly BankDbContext _dbContext;
+        public BalanceRepository(BankDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        /// <inheritdoc/>
+        public Task<decimal?> CheckBalanceAsync(Guid clientId)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc/>
+        public Task DepositIntoAsync(Guid clientId, decimal amount)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc/>
+        public Task WithDrawFromAsync(Guid clientId, decimal amount)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Repositories/ClientRepository.cs
@@ -0,0 +1,45 @@
+﻿using FinancialSettlementService.Dtos;
+using FinancialSettlementService.Interfaces;
+using FinancialSettlementService.Models;
+
+namespace FinancialSettlementService.Repositories
+{
+    /// <inheritdoc cref="IClientRepository"/>
+    public class ClientRepository : IClientRepository
+    {
+        /// <inheritdoc cref="BankDbContext"/>
+        private readonly BankDbContext _dbContext;
+        public ClientRepository(BankDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        /// <inheritdoc/>
+        public Task<ClientDto> GetByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc/>
+        public async Task SignUpAsync (ClientDto clientDto, CancellationToken cancellationToken)
+        {
+            var client = new Client
+            {
+                FirstName = clientDto.FirstName,
+                SecondName = clientDto.SecondName,
+                BirthDay = DateTime.Parse(clientDto.BirthDay),
+                Patronymic = clientDto.Patronymic,
+            };
+            var balanceAccount = new BalanceAccount
+            {
+                Balance = clientDto.Balance,
+                Client = client
+            };
+
+            await _dbContext.AddAsync(client, cancellationToken);
+            await _dbContext.AddAsync(balanceAccount, cancellationToken);
+
+            await _dbContext.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
